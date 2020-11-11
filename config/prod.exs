8437b584cc788833956ac4168b0d938a05012c0d
Patrick Tone <patrick@madronesoftware.com>
2020-11-11 13:07:54 +0100

    use the gigalixir url for the host

--- config/prod.exs
+++ config/prod.exs
@@ -11,7 +11,7 @@ use Mix.Config
 # before starting your production server.
 config :wake_up_web, WakeUpWeb.Endpoint,
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  url: [host: "example.com", port: 80],
+  url: [host: "sandybrown-deafening-xantus.gigalixirapp.com", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # ## SSL Support
