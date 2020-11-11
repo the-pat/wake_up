4e1c11e748637d87539eb78936f81b61ae1e6c89
Patrick Tone <patrick@madronesoftware.com>
2020-11-11 12:50:38 +0100

    stop using hsts

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,7 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :wake_up_web, WakeUpWeb.Endpoint,
-  force_ssl: [hsts: true],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
   url: [host: "example.com", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
